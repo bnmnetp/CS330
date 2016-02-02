05e68dee8ce0656ca6b450cad08185c074a83c51
Brad Miller <bonelake@mac.com>
2016-02-02 17:39:40 -0600

    fix for python3

--- 02_BasicNetwork/echoServerMP.py
+++ 02_BasicNetwork/echoServerMP.py
@@ -2,7 +2,7 @@
 import socket
 from multiprocessing import Process, Array
 
-        
+
 def run(conn):
     """docstring for run"""
     while 1:
@@ -10,7 +10,7 @@ def run(conn):
         if not data:
             break
         conn.send(data)
-    print 'closing connection ', conn
+    print('closing connection ', conn)
     conn.close()
 
 HOST = ''                 # Symbolic name meaning the local host
@@ -21,8 +21,6 @@ s.listen(2)               # listen param is length of backlog queue
 
 while True:
     newSock, addr = s.accept()
-    print 'Connected by', addr
+    print('Connected by', addr)
     p = Process(target=run, args=(newSock,))
     p.start()
-
-
