20ef87bf72f2d84196caa0dee6c00514a41171fa
Brad Miller <bonelake@mac.com>
2016-04-21 09:29:57 -0500

    decorator examples

---
+++ 09_wsgi/decotest.py
@@ -0,0 +1,57 @@
+def foodecorator(f):
+    print('about to wrap function')
+    def wrap(*args, **kwargs):
+        print('calling wrapped function')
+        return f(*args, **kwargs)
+
+    return wrap
+
+def foo(x,y,z=10):
+    return x + y + z
+
+foo = foodecorator(foo)
+
+print(foo(10,20,30))
+
+def bardecorator(x,y):
+    print("decorating with parameters")
+    def outerwrap(f):
+        print("outerwrap only takes f")
+        def wrap(*args, **kwargs):
+            print("captured x = {} and y = {}".format(x,y))
+            return f(*args, **kwargs)
+
+        return wrap
+    return outerwrap
+
+@bardecorator(20,30)
+def bar(a,b,c):
+    return a*b*c
+
+print('bar is defined')
+#bar = bardecorator(10,20)(bar)
+print('bar is now decorated',bar)
+print('calling bar')
+print(bar(2,3,4))
+
+class classdecorator:
+    def __init__(self,thefunc):
+        print('class decorating')
+        self.numCalls = 0
+        self.fun = thefunc
+
+    def __call__(self, *args, **kwargs):
+        print('preparing to call decorated func')
+        self.numCalls += 1
+        return self.fun(*args,**kwargs)
+
+
+@classdecorator
+def baz(a,b):
+    return a+b
+
+for i in range(20):
+    baz(i,20)
+
+print("baz was called {} times".format(baz.numCalls))
+
