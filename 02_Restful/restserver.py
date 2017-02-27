3a4947a215a339caf6dae508349bce72d2d36d34
Brad Miller <bonelake@mac.com>
2017-02-27 13:50:14 -0600

    wip restserver

--- 02_Restful/restserver.py
+++ 02_Restful/restserver.py
@@ -1,19 +1,34 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from json import dumps, loads
 
 app = Flask(__name__)
 api = Api(app=app)
 
-class Expenses(Resource):
+class Expense:
+    def __init__(self, date, store, category, item, amount ):
+        self.date = date
+        self.store = store
+        self.category = category
+        self.item = item
+        self.amount = float(amount)
+
+    def foo(self):
+        print('ffsf')
+        
+e = Expense("2017-02-22", "fareway", "grocery", "milk", "2.00")
+print(dumps(e.__dict__))
+
+class ExpenseResp(Resource):
 
     def get(self, expense_id=None):
         if expense_id == None:
-            return {'hello':'world'}
+            return dumps(e.__dict__)
         else:
             return {'hello': f'world {expense_id}'}
 
 
-api.add_resource(Expenses, '/api/v1/expenses',
+api.add_resource(ExpenseResp, '/api/v1/expenses',
                  '/api/v1/expenses/<int:expense_id>')
 
 if __name__ == '__main__':
@@ -24,4 +39,8 @@ if __name__ == '__main__':
 # /api/v1/expenses/356  -- return expense 346
 
 # POST
-# /api/v1/expenses  -- Create new expense data is in the data block
+# /api/v1/expenses  -- Create new expense, data is in the data block
+
+
+# DELETE
+# /api/v1/expenses/356 -- remove expense
